985563ccf5564b36a37ec2a4a595baf683341c18
YamasouA <akiakiskyhand@gmail.com>
2022-02-08 23:14:11 +0900

    Allow for-loops to define local variables

--- parse.c
+++ parse.c
@@ -581,7 +581,7 @@ bool is_typename() {
 // stmt = "return" expr ";" 
 //        | "if" "(" expr ")" stmt ("else" stmt)?
 //        | "while" "(" expr ")" stmt
-//        | "for" "(" expr? ";" expr? ";" expr? ")" stmt
+//        | "for" "(" (expr? ";" | declaration) expr? ";" expr? ")" stmt
 //        | "{" stmt* "}" 複数行のコードが記述
 //        | declaration
 //        | expr ";"
@@ -616,9 +616,17 @@ Node *stmt() {
   if (tok = consume("for")) {
     Node *node = new_node(ND_FOR, tok);
     expect("(");
+
+    VarScope *sc1 = var_scope;
+    TagScope *sc2 = tag_scope;
+
     if (!consume(";")) {
-      node->init = read_expr_stmt();
-      expect(";");
+      if (is_typename()) {
+        node->init = declaration(); // for (int i=0; ....)
+      } else {
+        node->init = read_expr_stmt(); // for (i=0; ...)
+        expect(";");
+      }
     }
     if (!consume(";")) {
       node->cond = expr();
@@ -629,6 +637,9 @@ Node *stmt() {
       expect(")");
     }
     node->then = stmt();
+
+    var_scope = sc1;
+    tag_scope = sc2;
     return node;
   }
 
